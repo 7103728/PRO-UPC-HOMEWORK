5b9fc3c6d06d976acd3a013ea30e10b3c52bdeb5
7103728 <141285168+7103728@users.noreply.github.com>
2023-11-07 21:12:31 -0400

    Create presentacion1.cs

---
+++ CodigoFacilito/presentacion1/presentacion1.cs
@@ -0,0 +1,13 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        static void main(string){
+            //Este es un programa de Hola Mundo
+            console.writeline("Hola Mundo");
+            global::System.Object value = console.readkey();
+        }
+    }
+}
+
+[] args)
