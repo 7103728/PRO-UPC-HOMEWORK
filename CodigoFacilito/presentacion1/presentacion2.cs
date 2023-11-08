e8819c7ff565254a6606d2133d75d417b9fc3c07
7103728 <141285168+7103728@users.noreply.github.com>
2023-11-07 22:11:41 -0400

    Create presentacion2.cs

---
+++ CodigoFacilito/presentacion1/presentacion2.cs
@@ -0,0 +1,24 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            int i = 0
+decimal x = 0.0 m;
+            float F = 0.0F;
+            double d = 0.0D;
+            string cadena = "Hola Mundo";
+            bool bandera = false;
+            datetime fecha = datetime.minvalue;
+            console.writeline("El valor de i es:{0}", i);
+            console.writeline("El valor de x es:{0:c}", x);
+            console.writeline("El valor de f es:{0:f2}", f);
+            console.writeline("El valor de d es:{0:f2}", d);
+            console.writeline("El valor de cadena es: " + cadena);
+            console.writeline("El valor de bandera es: " + bandera.tostring());
+            console.writeline("El valor de fecha es: " + fecha.toshortdatestring());
+            console readkey();
+
+        }
+    }
+}
